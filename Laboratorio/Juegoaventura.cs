038384f033796228e6a5616a005c5230970d01bd
Bryan Sandoval <bsandovalr1@miumg.edu.gt>
2024-04-20 12:48:03 -0600

    primer

---
+++ Laboratorio/Juegoaventura.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorioProgra1.Laboratorio
+{
+    internal class Juegoaventura
+    {
+
+        public static void saludo()
+        {
+            Console.WriteLine("Hi Gamer:");
+        }
+
+
+        public void IniciarPartida()
+        {
+            jugando = true;
+            Console.WriteLine("¡Partida iniciada!");
+        }
+
+        public void DetenerPartida()
+        {
+            // Error lógico
+            jugando = false;//faltaba el false de jugando para indicar de que termino.
+            Console.WriteLine("Partida detenida.");
+        }
+
+    }
+}
